fc9a022cdab29e7efc9e16042c509ead721bda7c
ManOfBlessing225 <adigraemmanuel@yahoo.com>
2022-03-12 10:03:24 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/*
+ * Main is the entry point of the programming in C
+ *
+ * This program aims to print with puts
+ */
 
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
